6b50f4f7d255bd8b036e1025d43f750c0af6f62a
harishkumar-morgan <134667507+harishkumar-morgan@users.noreply.github.com>
2023-07-24 11:29:23 +0530

    added reports part

--- src/test/java/stepDef/Hooks.java
+++ src/test/java/stepDef/Hooks.java
@@ -3,7 +3,11 @@ package stepDef;
 import utils.helper.*;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
- 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import io.cucumber.java.Scenario;
+
+
 public class Hooks {
      
     @Before
@@ -13,7 +17,13 @@ public class Hooks {
     }
      
     @After
-    public static void tearDown() {
+    public static void tearDown(Scenario scenario) {
+ 
+        //validate if scenario has failed
+        if(scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName()); 
+        }   
          
         HelperClass.tearDown();
     }
